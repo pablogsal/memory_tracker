fcb5a47b32b963e63a02d8f9b963fff138977407
Marta Gomez <mgmacias@google.com>
2025-06-16 20:39:59 +0200

    clean

--- frontend/.next/static/chunks/src_app_diff_page_tsx_56376f92._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/src_85a6fd63._.js",
-  "static/chunks/node_modules_93be0bdf._.js"
-],
-    source: "dynamic"
-});
